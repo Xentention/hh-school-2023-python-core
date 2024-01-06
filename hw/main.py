2fd176088072367c2675b45c8dbeb3d8a6495e9e
Xenia <xentimoshenko@gmail.com>
2024-01-06 21:48:58 +0300

    Fixed: now None values are safe to use

--- hw/main.py
+++ hw/main.py
@@ -6,12 +6,14 @@ from datetime import date
 wines = [
     Wine("wine1", date.fromisoformat('2019-12-04')), 
     Wine("wine2", date.fromisoformat('2020-12-04')), 
+    Wine(),
     Wine("wine3", date.fromisoformat('2021-12-04'))
 ]
 
 beers = [
     Beer("beer1", date.fromisoformat('2019-12-04')), 
     Beer("beer2", date.fromisoformat('2020-12-04')), 
+    Beer("beer_none"),
     Beer("beer3", date.fromisoformat('2021-12-04'))
 ]
 
@@ -20,6 +22,7 @@ market = Market(wines, beers)
 print('Has drink:')
 print(f'Should be true: {market.has_drink_with_title("wine1")}')
 print(f'Should be false: {market.has_drink_with_title("wine5")}')
+print(f'None arg: {market.has_drink_with_title("")}')
 
 print('\n<------------>\n')
 
@@ -29,6 +32,8 @@ print(market.get_drinks_sorted_by_title())
 print('\n<------------>\n')
 
 print('Get by prod date:')
-print(f"All: {market.get_drinks_by_production_date(date.fromisoformat('2019-12-04'),  date.fromisoformat('2023-12-04'))}")
+print(f"All where prod_date isn't None: {market.get_drinks_by_production_date(date.fromisoformat('2019-12-04'),  date.fromisoformat('2023-12-04'))}")
+print(f"All where prod_date isn't None but date_to is None: {market.get_drinks_by_production_date(date.fromisoformat('2019-12-04'))}")
+print(f"None args: {market.get_drinks_by_production_date()}")
 print(f"1 Wine 1 Beer: {market.get_drinks_by_production_date(date.fromisoformat('2019-12-04'),  date.fromisoformat('2019-12-05'))}")
 print(f"None: {market.get_drinks_by_production_date(date.fromisoformat('2023-12-04'),  date.fromisoformat('2023-12-04'))}")
--- hw/market.py
+++ hw/market.py
@@ -1,18 +1,22 @@
 class Market:
 
     def __init__(self, wines: list = None, beers: list = None) -> None:
-        drinks_list = wines + beers
+        drinks_list = ([] if wines is None else wines) + ([] if beers is None else beers)
         self.drinks = dict((d.title, d) for d in drinks_list)
 
     def has_drink_with_title(self, title=None) -> bool:
         return title in self.drinks
 
     def get_drinks_sorted_by_title(self) -> list:
-        return list(dict(sorted(self.drinks.items())).values())
+        return list(dict(sorted(self.drinks.items(), key = lambda k: (k[0] is None, k[0]))).values())
 
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
         def check_date(pair) -> bool:
             key, value = pair
-            return from_date <= value.production_date <= to_date;
-
+            if value.production_date is None:
+                return False; 
+            return from_date <= value.production_date and (to_date is None or value.production_date <= to_date);
+    
+        if from_date is None:
+            return []
         return list(dict(filter(check_date, self.drinks.items())).values())
